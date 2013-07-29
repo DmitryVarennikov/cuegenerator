e7841a02f3a20664500bffb30749eacc7b143fda
dV <dVaffection@gmail.com>
2013-07-29 04:19:39 +0000

    Got rif of olb useless db code

--- index.php
+++ index.php
@@ -1,32 +1,10 @@
 <?php
 require_once '_config.php';
-require_once '_kernel/utils.inc.php';
 
 function increment_counter() 
 {
     file_put_contents(COUNTER_FILENAME, 
         (int)file_get_contents(COUNTER_FILENAME) + 1);
-}
-
-function log_cue() 
-{
-    require_once '_kernel/Db.php';
-    require_once '_kernel/Db/Exception.php';
-    
-    try {
-        database::connect();
-        
-        $affected_rows = database::replace('cues', array_extract($_POST, 
-            array('perfomer', 'title', 'filename', 'tracklist', 'regions_list', 'cue')));
-        
-    } catch(Kernel_Db_Exception $exception) {
-        if (DEBUG_MODE) {
-            var_dump($exception->getMessage());
-            exit;
-        }
-    }
-    
-    return $affected_rows;
 }
 
 
@@ -44,9 +22,7 @@ if ('post' == strtolower($_SERVER['REQUEST_METHOD']) &&
     $meta_refresh = '<meta http-equiv="refresh" content="1; url=/cue.php" />';
     
     
-    //if (log_cue() == 1) {
-        increment_counter();
-    //}
+    increment_counter();
 } 
 
 require_once 'index.html';
