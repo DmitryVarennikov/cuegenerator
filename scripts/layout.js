deb9fc992120e92671195dee659b984279814971
dV <dVaffection@gmail.com>
2014-10-19 19:02:03 -0700

    Closed #2: Added the link to the chrome extension download page if needed

--- scripts/layout.js
+++ scripts/layout.js
@@ -2,6 +2,7 @@ define([
     'jquery'
 ], function () {
 
+    // stretching text areas
     var height = $(document).height() - 20;
     $('#tracklist').animate({height: height - 350});
     $('#cue').animate({height: height - 173});
@@ -9,5 +10,22 @@ define([
     $('#cue').one('click', function () {
         $(this).select();
     });
+
+    // showing a link to the chrome extension download page if needed
+    var isChromeBrowser = navigator.userAgent.toLowerCase().indexOf('chrome') > - 1;
+    if (isChromeBrowser) {
+        var manifest = document.createElement('script');
+        manifest.src = 'chrome-extension://phoaoafhahilmaoddhppejobcmgnglmc/manifest.json';
+        document.body.appendChild(manifest);
+        // show if extension manifest can not be loaded
+        manifest.onerror = function () {
+            var chromeExtension = document.getElementById('chrome-extension');
+            chromeExtension.style.display = 'block';
+
+            // calculate button left corner
+            var left = Math.round((document.body.offsetWidth - chromeExtension.offsetWidth) / 2);
+            chromeExtension.style.left = left + 'px';
+        };
+    }
 
 });
