ed16d6116c13ebbcb055becfbb3ccdb9b06d904e
Dmitry Varennikov <dmitry.varennikov@gmail.com>
2021-02-25 22:33:14 -0500

    No chrome ext, no G plus author

--- scripts/layout.js
+++ scripts/layout.js
@@ -11,21 +11,4 @@ define([
         $(this).select();
     });
 
-    // showing a link to the chrome extension download page if needed
-    var isChromeBrowser = navigator.userAgent.toLowerCase().indexOf('chrome') > - 1;
-    if (isChromeBrowser) {
-        var manifest = document.createElement('script');
-        manifest.src = 'chrome-extension://phoaoafhahilmaoddhppejobcmgnglmc/manifest.json';
-        document.body.appendChild(manifest);
-        // show if extension manifest can not be loaded
-        manifest.onerror = function () {
-            var chromeExtension = document.getElementById('chrome-extension');
-            chromeExtension.style.display = 'block';
-
-            // calculate button left corner
-            var left = Math.round((document.body.offsetWidth - chromeExtension.offsetWidth) / 2);
-            chromeExtension.style.left = left + 'px';
-        };
-    }
-
 });
